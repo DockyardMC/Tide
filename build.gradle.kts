46a923239018776720f50ce9afad3181148293be
LukynkaCZE <jiraklukas058@seznam.cz>
2025-04-12 00:24:33 +0200

    publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,15 @@
+import java.net.URI
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse
+
 plugins {
+    `maven-publish`
     kotlin("jvm") version "2.1.0"
 }
 
 group = "io.github.dockyard.tide"
-version = "1.0"
+version = properties["tide.version"]!!
 
 repositories {
     mavenCentral()
@@ -24,4 +30,93 @@ tasks.test {
 }
 kotlin {
     jvmToolchain(21)
+}
+
+tasks.withType<PublishToMavenRepository> {
+    if(!version.toString().endsWith("-SNAPSHOT")) {
+        dependsOn("test")
+    }
+}
+
+java {
+    withSourcesJar()
+    withJavadocJar()
+}
+
+publishing {
+    repositories {
+        maven {
+            url = if(version.toString().endsWith("-SNAPSHOT")) {
+                uri("https://mvn.devos.one/snapshots")
+            } else {
+                uri("https://mvn.devos.one/releases")
+            }
+            credentials {
+                username = System.getenv()["MAVEN_USER"]
+                password = System.getenv()["MAVEN_PASS"]
+            }
+        }
+    }
+    publications {
+        register<MavenPublication>("maven") {
+            groupId = "io.github.dockyardmc"
+            artifactId = "tide"
+            version = version.toString()
+            from(components["java"])
+
+        }
+    }
+}
+
+tasks.publish {
+    finalizedBy("sendPublishWebhook")
+}
+
+task("sendPublishWebhook") {
+    group = "publishing"
+    description = "Sends a webhook message after publishing to Maven."
+
+    doLast {
+        sendWebhookToDiscord(System.getenv("DISCORD_DOCKYARD_WEBHOOK"))
+    }
+}
+
+fun sendWebhookToDiscord(webhookUrl: String) {
+    val httpClient = HttpClient.newHttpClient()
+
+    val requestBody = embed()
+    val request = HttpRequest.newBuilder()
+        .uri(URI(webhookUrl))
+        .header("Content-Type", "application/json")
+        .POST(HttpRequest.BodyPublishers.ofString(requestBody))
+        .build()
+
+    httpClient.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+
+        .thenRun { println("Webhook sent successfully!") }
+        .exceptionally { throwable ->
+            throwable.printStackTrace()
+            null
+        }
+}
+
+fun embed(): String {
+    val target = if(version.toString().endsWith("-SNAPSHOT")) "https://mvn.devos.one/snapshots" else "https://mvn.devos.one/releases"
+    val color = if(version.toString().endsWith("-SNAPSHOT")) 16742912 else 65290
+    val title = if(version.toString().endsWith("-SNAPSHOT")) "Snapshot Published to Maven" else "Published to Maven"
+    return """
+        {
+          "content": null,
+          "embeds": [
+            {
+              "title": "$title",
+              "description": "`io.github.dockyardmc:tide:$version` was successfully published to maven **$target**!",
+              "color": $color
+            }
+          ],
+          "username": "Mavenboo",
+          "avatar_url": "https://img.lukynka.cloud/live_bangboo_reaction.webp",
+          "attachments": []
+        }
+    """.trimIndent()
 }
